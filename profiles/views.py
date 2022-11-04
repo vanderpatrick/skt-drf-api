d80aecf6bc0f58895080e2fc0e9d8e36355dea4c
vanderpatrick <patrick123321123@icloud.com>
2022-11-04 09:22:41 +0000

    Add profile detail to views and set up path

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,15 @@
 from rest_framework import generics
 from .models import Profile
 from .serializers import ProfileSerializer
+from skt_drf_api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(generics.ListAPIView):
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
+
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
