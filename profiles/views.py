931ab13ad6971b62780318b0a1aa6f5f95041837
vanderpatrick <patrick123321123@icloud.com>
2022-11-04 09:54:20 +0000

    Add more fields to profile serializer and views

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,17 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from .models import Profile
+from django.db.models import Count
 from .serializers import ProfileSerializer
 from skt_drf_api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(generics.ListAPIView):
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
