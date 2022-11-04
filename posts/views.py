52ec0640d3dc2a2c12325d31a8ed9f53884610a7
vanderpatrick <patrick123321123@icloud.com>
2022-11-04 09:33:47 +0000

    Add post views, serializers path and create likes app

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import status, permissions, generics, filters
+from .models import Post
+from .serializers import PostSerializer
+from skt_drf_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    permissions_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
