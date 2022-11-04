d2ceaa420d5ea95a04d8ab403db8dcb97d3dbfc0
vanderpatrick <patrick123321123@icloud.com>
2022-11-04 10:01:02 +0000

    Add filters to profile app and post_save to create new profiles

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
@@ -10,7 +10,8 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_PIC', blank=True)
+        upload_to='images/', default='../default_profile_gslre8.jpg'
+    )
 
     class Meta:
         ordering = ['-created_at']
@@ -22,3 +23,6 @@ class Profile(models.Model):
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from .models import Profile
 from django.db.models import Count
 from .serializers import ProfileSerializer
 from skt_drf_api.permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ProfileList(generics.ListAPIView):
@@ -13,8 +14,29 @@ class ProfileList(generics.ListAPIView):
         following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')
 
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend
+    ]
+
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
+
+    filterset_fields = [
+        'owner__followed__owner__profile',
+    ]
+
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
