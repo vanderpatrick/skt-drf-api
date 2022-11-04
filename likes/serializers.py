de49a8126e8c2a3a1516255b4b3af4dbfddb2d57
vanderpatrick <patrick123321123@icloud.com>
2022-11-04 09:39:25 +0000

    Create likes app, add like module, views, serializers and path

---
+++ likes/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from django.db import IntegrityError
+from .models import Like
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Like
+        fields = [
+            'owner', 'post', 'created_at', 'id'
+        ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
--- skt_drf_api/settings.py
+++ skt_drf_api/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
     'profiles',
     'posts',
+    'likes',
 ]
 
 MIDDLEWARE = [
