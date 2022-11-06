7299ce9fb792fadbfc51ee876caea170357dd652
vanderpatrick <patrick123321123@icloud.com>
2022-11-06 08:22:51 +0000

    Remove else statement from CLient_origin

--- skt_drf_api/settings.py
+++ skt_drf_api/settings.py
@@ -79,10 +79,6 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN'),
     ]
-else:
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
-    ]
 
 CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [os.environ.get(
