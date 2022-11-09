7926ab446388e1d3be2a1fd93a468afd47e2addf
vanderpatrick <patrick123321123@icloud.com>
2022-11-09 13:43:38 +0000

    Make content field required in post model

--- posts/models.py
+++ posts/models.py
@@ -28,7 +28,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
+    content = models.TextField()
     image = models.ImageField(
         upload_to='images/', default='../default_profile_qdjgyp', blank=True
     )
