9ad185a01a1915611af7bc2e492a69fc860da18f
vanderpatrick <patrick123321123@icloud.com>
2022-11-15 16:45:53 +0000

    Add logout route

--- skt_drf_api/views.py
+++ skt_drf_api/views.py
@@ -1,8 +1,34 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+
 @api_view()
 def root_route(request):
     return Response({
         "message": "welcome to the for real"
     })
+
+
+@api_view(['POST'])
+def logout_route(request):
+    """dj-rest-auth-logout-view-fix"""
+    response = Response()
+    response.set_cookie(
+        key=JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    response.set_cookie(
+        key=JWT_AUTH_REFRESH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    return response
