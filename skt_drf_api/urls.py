62bded5da04bfd80fc47aeebf103f393d223ec90
vanderpatrick <patrick123321123@icloud.com>
2022-12-13 16:19:03 +0000

    Add followers path to drf urls

--- skt_drf_api/urls.py
+++ skt_drf_api/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
     path('', include('likes.urls')),
+    path('', include('followers.urls')),
     path('', include('comments.urls')),
     path('', include('postvideo.urls')),
     path('', include('dislike.urls')),
