1cdb14549f4cb9d35dbeaa7373f0bea57d4b6836
vanderpatrick <patrick123321123@icloud.com>
2022-12-14 17:32:11 +0000

    Add annote to video list queryset

--- videos/views.py
+++ videos/views.py
@@ -25,4 +25,8 @@ class VideoList(generics.ListCreateAPIView):
 class VideoDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = VideoSerializer
-    queryset = Videos.objects.all().order_by('-created_at')
+    queryset = Videos.objects.annotate(
+        video_comments=Count('videocomments', distinct=True),
+        video_likes=Count('videoslike', distinct=True),
+        video_dislike=Count('videodislike', distinct=True)
+    ).order_by('-created_at')
