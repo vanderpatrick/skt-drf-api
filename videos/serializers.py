edf0c176a746d87219681bb352bc7ca1d3bfaae1
vanderpatrick <patrick123321123@icloud.com>
2022-12-14 20:39:58 +0000

    Add get like/dislike id for video serializer

--- videos/serializers.py
+++ videos/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from videos.models import Videos
+from videolike.models import VideoLike
+from videodislike.models import VideoDislike
 
 
 class VideoSerializer(serializers.ModelSerializer):
@@ -9,11 +11,31 @@ class VideoSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     video_comments = serializers.ReadOnlyField()
     video_likes = serializers.ReadOnlyField()
+    video_like_id = serializers.SerializerMethodField()
     video_dislike = serializers.ReadOnlyField()
+    video_dislike_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_video_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            videolike = VideoLike.objects.filter(
+                owner=user, video=obj
+            ).first()
+            return videolike.id if videolike else None
+        return None
+
+    def get_video_dislike_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            video_dislike = VideoDislike.objects.filter(
+                owner=user, video=obj
+            ).first()
+            return video_dislike.id if video_dislike else None
+        return None
 
     class Meta:
         model = Videos
@@ -30,5 +52,7 @@ class VideoSerializer(serializers.ModelSerializer):
             'post_location',
             'video_comments',
             'video_likes',
-            'video_dislike'
+            'video_dislike',
+            'video_like_id',
+            'video_dislike_id'
         ]
