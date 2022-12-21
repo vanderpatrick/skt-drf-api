29646cf25d2cdc1bcbfa198a426da07a25e66a35
vanderpatrick <patrick123321123@icloud.com>
2022-12-21 15:44:33 +0000

    Add profile video counter to filter

--- profiles/views.py
+++ profiles/views.py
@@ -24,6 +24,7 @@ class ProfileList(generics.ListAPIView):
     ]
     ordering_fields = [
         'posts_count',
+        'videos_count',
         'followers_count',
         'following_count',
         'owner__following__created_at',
